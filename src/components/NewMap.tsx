caaa28dc118096becb54963ae05e0c765f77e15d
albertincx <albertincx@gmail.com>
2024-07-06 11:37:55 +0700

    deploy fix

--- src/components/NewMap.tsx
+++ src/components/NewMap.tsx
@@ -7,7 +7,7 @@ import "leaflet-routing-machine";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 
 import 'leaflet/dist/leaflet.css'
-import {SearchControl, OpenStreetMapProvider} from 'react-leaflet-geosearch';
+// import {SearchControl, OpenStreetMapProvider} from 'react-leaflet-geosearch';
 
 var pin;
 window.bindMarkerRouteCab1 = function (ev, map, setValue) {
@@ -34,6 +34,7 @@ const addPoint = (map, point, setValue) => {
     pin.addTo(map);
     map.setView([point.lat, point.lng], 13);
 }
+// @ts-ignore
 const Map: FC = ({ setValue, point }) => {
     const [map, setMap] = useState(null);
     const getPosition = (map) => {
@@ -55,12 +56,15 @@ const Map: FC = ({ setValue, point }) => {
     useEffect(() => {
         if (map) {
             getPosition(map)
+            // @ts-ignore
             map.on('click', function (ev) {
                 window.bindMarkerRouteCab1(ev, map, setValue)
             });
             return () => {
                 if (map) {
+                    // @ts-ignore
                     map.off();
+                    // @ts-ignore
                     map.remove();
                 }
             }
@@ -69,29 +73,29 @@ const Map: FC = ({ setValue, point }) => {
     }, [map])
     return (
         <div id='map1' style={{width: '100%', height: '70%'}}>
-            <MapContainer whenCreated={setMap} zoom={13}>
+            <MapContainer whenReady={() => setMap}>
                 <TileLayer
                     url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    // attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <SearchControl
-                    provider={OpenStreetMapProvider()}
-                    showMarker={false}
-                    showPopup={false}
-                    popupFormat={({query, result}) => {
-                        window.bindMarkerRouteCab1({latlng: {lat: result.y, lng: result.x}}, map, setValue)
-                        return result.label
-                    }}
-                    resultFormat={({query, result}) => {
-                        return result.label
-                    }}
-                    maxMarkers={1}
-                    retainZoomLevel={true}
-                    animateZoom={true}
-                    autoClose={false}
-                    searchLabel={"Enter address, please"}
-                    keepResult={true}
-                />
+                {/*<SearchControl*/}
+                {/*    provider={OpenStreetMapProvider()}*/}
+                {/*    showMarker={false}*/}
+                {/*    showPopup={false}*/}
+                {/*    popupFormat={({query, result}) => {*/}
+                {/*        window.bindMarkerRouteCab1({latlng: {lat: result.y, lng: result.x}}, map, setValue)*/}
+                {/*        return result.label*/}
+                {/*    }}*/}
+                {/*    resultFormat={({query, result}) => {*/}
+                {/*        return result.label*/}
+                {/*    }}*/}
+                {/*    maxMarkers={1}*/}
+                {/*    retainZoomLevel={true}*/}
+                {/*    animateZoom={true}*/}
+                {/*    autoClose={false}*/}
+                {/*    searchLabel={"Enter address, please"}*/}
+                {/*    keepResult={true}*/}
+                {/*/>*/}
             </MapContainer>
         </div>
     )
