7182a3e71689dfc989956429cb7fa98338009e9a
Albert Safiullin <albertincx@gmail.com>
2025-06-09 21:54:28 +0500

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
-import { defineConfig } from "vite";
+// @ts-ignore
+import path from 'path';
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 // @ts-ignore
 import ta from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+// @ts-ignore
+export default ({mode}) => defineConfig({
     plugins: [react(), ta()],
+    resolve: {
+        alias: {
+            // @ts-ignore
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    esbuild: mode === 'production' ? {
+        drop: ['console', 'debugger'],
+    } : {
+        supported: {
+            'top-level-await': true
+        },
+    },
+    optimizeDeps: mode !== 'production' ? {
+        esbuildOptions: {
+            supported: {
+                "top-level-await": true
+            },
+        },
+    } : {},
 });
